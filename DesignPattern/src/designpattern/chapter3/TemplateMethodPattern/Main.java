a09bc107265034f9488036d1e3ed7dc3e8c04299
HyangKeun Choi <gidrms99@naver.com>
2020-01-03 16:08:54 +0900

    java design pattern

---
+++ DesignPattern/src/designpattern/chapter3/TemplateMethodPattern/Main.java
@@ -0,0 +1,15 @@
+package designpattern.chapter3.TemplateMethodPattern;
+
+import designpattern.chapter3.dp.AbstGameConnectHelper;
+import designpattern.chapter3.dp.DefaultGameConnectHelper;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		AbstGameConnectHelper helper = new DefaultGameConnectHelper();
+		
+		
+		helper.requestConnection("아이디 암호 등 접속 접보");
+		
+	}
+}
